7fb6d2f8eb57ec61d1ab33f985f677af3648aabd
kangaroojun <kangjun_ong@mymail.sutd.edu.sg>
2024-06-22 11:49:14 +0800

    fix: html connections to scss files, rename iocns to icons

--- Backend/app/assets/config/manifest.js
+++ Backend/app/assets/config/manifest.js
@@ -3,3 +3,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link sidebar.js
+//= link documentuploadpage/application_documentuploadpage.css
+//= link applicationformpage/application_formpage.css
+//= link applypage/application_applypage.css
