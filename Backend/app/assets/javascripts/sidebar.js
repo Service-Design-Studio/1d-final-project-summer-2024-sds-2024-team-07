df83699f342cf846a04b5fd3acaf01791f4ae9d7
Azfar Yusri <azfar200@gmail.com>
2024-07-04 18:08:08 +0800

    azfar

--- Backend/app/assets/javascripts/sidebar.js
+++ Backend/app/assets/javascripts/sidebar.js
@@ -62,8 +62,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-
-const button1 = document.getElementById("button1")
-const button2 = document.getElementById("button1")
-const button3 = document.getElementById("button1")
