886354585e769b13d99173029f06ac2ca715ac88
kangaroojun <kangjun_ong@mymail.sutd.edu.sg>
2024-07-26 21:56:33 +0800

    feat: applicationchecklist rspec singaporean

--- Frontend/spec/views/pages/applicationchecklist.html.erb_spec.rb
+++ Frontend/spec/views/pages/applicationchecklist.html.erb_spec.rb
@@ -7,39 +7,56 @@ RSpec.feature "ApplicationChecklist", type: :feature do
     find('div#section1').click # Click on "Principal Credit Card" section
 
     within('#label1.tab-content') do
-      expect(page).to have_content('Identification Document')
-      expect(page).to have_css("img[src*='Nric-c859373acea659332f601402c8738708f67598479ea4e4ebf6df29bc47252af8.png']", visible: true)
+      expect(page).to have_content('NRIC (front & back) for Singaporean/Permanent Residents')
+      expect(page).to have_css("img.main-image", visible: true)
     end
 
     find('button.accordion-header', text: 'Financial Document').click
-
-    within('#label2.tab-content') do
-      expect(page).to have_no_content('Identification Document')
-      expect(page).to have_content('Financial Document')
-      expect(page).to have_css("img[src*='income-slip-a9663ea0923f09a528b4e6c212a252fc7cd358888d467a5c9061a1ea876570b7.png']", visible: true)
-    end
-
-    find('.next').click
-    within('#label2.tab-content') do
-      expect(page).to have_css("img[src*='cpf-contribution-history-c4e4d31275c54d803677fccd98b882bd98a7a29fb472d7ac88ee7612f9cdc2ea.jpg']", visible: true)
-    end
-
-    find('.prev').click
-    within('#label2.tab-content') do
-      expect(page).to have_css("img[src*='income-slip-a9663ea0923f09a528b4e6c212a252fc7cd358888d467a5c9061a1ea876570b7.png']", visible: true)
+    within('#label1.tab-content') do
+      expect(page).to have_content("Latest 3 months’ computerised payslip in SGD is preferred; else minimally Latest 1 month’s computerised payslip")
+      expect(page).to have_content("Latest 12 months’ CPF Contribution History Statement")
+      expect(page).to have_content("Latest 3 months’ salary crediting bank statements in SGD")
+      expect(page).to have_css("img", visible: true)
     end
 
     find('button.accordion-header', text: 'Income Tax Notice of Assessment').click
-    within('#label3.tab-content') do
-      expect(page).to have_no_content('Financial Document')
-      expect(page).to have_content('Income Tax Notice of Assessment')
-      expect(page).to have_css("img[src*='tax-assesment-dda2ef0e856cecb0f63c77f0afec41287953466de0bf7cba351a45850c75a133.png']", visible: true)
+    within('#label1.tab-content') do
+      expect(page).to have_content('Latest 2 years of Income Tax Notice of Assessment in SGD is preferred; else minimally latest 1 year of Income Tax Notice of Assessment')
+      expect(page).to have_css("img.main-image", visible: true)
+    end
+  end
+  scenario "Viewing documents required for Singaporean salaried employee (less than 3 months)" do
+    visit pages_applicationchecklist_path
+
+    find('button', text: 'Salaried Employee (less than 3 months)').click # Click on "Principal Credit Card" section
+
+    within('#label1.tab-content') do
+      expect(page).to have_content('NRIC (front & back) for Singaporean/Permanent Residents')
+      expect(page).to have_css("img.main-image", visible: true)
     end
 
-    find('span.close').click
-    expect(page).to have_current_path(pages_applicationchecklist_path)
+    find('button.accordion-header', text: 'Financial Document').click
+    within('#label1.tab-content') do
+      expect(page).to have_content("Latest 3 months’ computerised payslip in SGD is preferred; else minimally Latest 1 month’s computerised payslip")
+      expect(page).to have_content("Latest 12 months’ CPF Contribution History Statement")
+      expect(page).to have_content("Latest 3 months’ salary crediting bank statements in SGD")
+      expect(page).to have_css("img", visible: true)
+    end
   end
+  scenario "Viewing documents required for Singaporean Variable/Commission-based Employees or Self-Employed" do
+    visit pages_applicationchecklist_path
 
-  # Add other scenarios similarly, scoping within specific elements as needed.
+    find('button', text: 'Variable/Commission-based Employees or Self-Employed').click # Click on "Principal Credit Card" section
 
+    within('#label1.tab-content') do
+      expect(page).to have_content('NRIC (front & back) for Singaporean/Permanent Residents')
+      expect(page).to have_css("img.main-image", visible: true)
+    end
+
+    find('button.accordion-header', text: 'Income Tax Notice of Assessment').click
+    within('#label1.tab-content') do
+      expect(page).to have_content('Latest 2 years of Income Tax Notice of Assessment in SGD is preferred; else minimally latest 1 year of Income Tax Notice of Assessment')
+      expect(page).to have_css("img.main-image", visible: true)
+    end
+  end
 end
