781f7c96047a8de7a5e8b686a68535621ed53d70
yuhueng <144983073+yuhueng@users.noreply.github.com>
2024-08-05 17:39:12 +0800

    Fix: Moved .github folder to Root Directory. Updated Services/Google Cloud Storage and Dockerfile (For GithubActions)

--- Frontend/app/services/google_cloud_storage_service.rb
+++ Frontend/app/services/google_cloud_storage_service.rb
@@ -1,15 +1,15 @@
-# class GoogleCloudStorageService
-#   def initialize
-#     # Hardcoded bucket name
-#     @bucket_name = "dbs-development-bucket"
-#     @bucket = $storage.bucket(@bucket_name)
-#     raise "Bucket not found" unless @bucket
-#   end
+class GoogleCloudStorageService
+  def initialize
+    # Hardcoded bucket name
+    @bucket_name = "dbs-development-bucket"
+    @bucket = $storage.bucket(@bucket_name)
+    raise "Bucket not found" unless @bucket
+  end
 
-#   def upload_file(file_path)
-#     raise "Bucket not found" unless @bucket
+  def upload_file(file_path)
+    raise "Bucket not found" unless @bucket
 
-#     file_name = File.basename(file_path)
-#     @bucket.create_file(file_path, file_name)
-#   end
-# end
+    file_name = File.basename(file_path)
+    @bucket.create_file(file_path, file_name)
+  end
+end
